217a44e9a3b50b62fabce448756a73e60476f626
gurpreetkaur <gurpreetalliswell@gmail.com>
2023-04-21 17:47:17 +0930

    match draw message

--- tictac.js
+++ tictac.js
@@ -18,42 +18,38 @@ board.addEventListener('click', function(event){
 				console.log(" X player Won!!!")
 			}else if(box2.textContent ==="X" && box5.textContent==="X" && box8.textContent==="X"){
 				console.log(" X player Won!!!")
-			}else if(box3.textContent ==="X" && box6.textContent==="X" && box9.textContent==="X"){0.
+			}else if(box3.textContent ==="X" && box6.textContent==="X" && box9.textContent==="X"){
 				console.log(" X player Won!!!")
 			}else if(box1.textContent ==="X" && box5.textContent==="X" && box9.textContent==="X"){
 				console.log(" X player Won!!!")
 			}else if(box3.textContent ==="X" && box5.textContent==="X" && box7.textContent==="X"){
 				console.log(" X player Won!!!")
 			}
+	}else if(clickedBox=='' && clicks%2===0){
+			console.log("Match is draw!!")
 	}else{
 			clickedBox.textContent="O"
-			if(box1.textContent ==="O" && box2.textContent==="O" && box3.textContent==="O"){
-				console.log(" O player Won!!!")
-			}else if(box4.textContent ==="O" && box5.textContent==="O" && box6.textContent==="O"){
-				console.log(" O player Won!!!")
-			}else if(box7.textContent ==="O" && box8.textContent==="O" && box9.textContent==="O"){
-				console.log(" O player Won!!!")
-			}else if(box1.textContent ==="O" && box4.textContent==="O" && box7.textContent==="O"){
-				console.log(" O player Won!!!")
-			}else if(box2.textContent ==="O" && box5.textContent==="O" && box8.textContent==="O"){
-				console.log(" O player Won!!!")
-			}else if(box3.textContent ==="O" && box6.textContent==="O" && box9.textContent==="O"){
-				console.log(" O player Won!!!")
-			}else if(box1.textContent ==="O" && box5.textContent==="O" && box9.textContent==="O"){
-				console.log(" O player Won!!!")
-			}else if(box3.textContent ==="O" && box5.textContent==="O" && box7.textContent==="O"){
-				console.log(" O player Won!!!")
-			}
-	
-	if(clickedBox.textContent=="X" ||clickedBox.textContent=="O" ){
-			console.log("Use another box!")
+				if(box1.textContent ==="O" && box2.textContent==="O" && box3.textContent==="O"){
+					console.log(" O player Won!!!")
+				}else if(box4.textContent ==="O" && box5.textContent==="O" && box6.textContent==="O"){
+					console.log(" O player Won!!!")
+				}else if(box7.textContent ==="O" && box8.textContent==="O" && box9.textContent==="O"){
+					console.log(" O player Won!!!")
+				}else if(box1.textContent ==="O" && box4.textContent==="O" && box7.textContent==="O"){
+					console.log(" O player Won!!!")
+				}else if(box2.textContent ==="O" && box5.textContent==="O" && box8.textContent==="O"){
+					console.log(" O player Won!!!")
+				}else if(box3.textContent ==="O" && box6.textContent==="O" && box9.textContent==="O"){
+					console.log(" O player Won!!!")
+				}else if(box1.textContent ==="O" && box5.textContent==="O" && box9.textContent==="O"){
+					console.log(" O player Won!!!")
+				}else if(box3.textContent ==="O" && box5.textContent==="O" && box7.textContent==="O"){
+					console.log(" O player Won!!!")
 				}
-	}	
-			clicks= clicks+1;
-	
-	
-
 	}
+	
+			clicks= clicks+1;
+
 })
  //var vertical = document.querySelector('')
 
