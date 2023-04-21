8acba4a2d2467ad6adf91e0c686bdd9a9d2f20c9
gurpreetkaur <gurpreetalliswell@gmail.com>
2023-04-21 13:42:28 +0930

    adding O

--- tictac.js
+++ tictac.js
@@ -3,19 +3,18 @@
 var board = document.querySelector('.board');
 var clicks = 0
 board.addEventListener('click', function(event){
-	console.log(event.target.textContent)
+	console.log(event.target)
 	var clickedBox= event.target
+	//for(var i =0 ; i<)
+	
 	if(clickedBox!==''){
 		clicks= clicks+1;
-		if(clicks!==2){
-		clickedBox.textContent = "X";
-		}else{
-			clickedBox.textContent = "O";
-		}
+		console.log(clicks)
 	}
-	
+	clickedBox.textContent = "X";
+	if(clicks%=2){
+		clickedBox.textContent="O"
+	}
 })
-	// click.style.background ='red'
-	// 
-		
+	
 		
