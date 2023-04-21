aaa387b764ccef1a05980fdee73883eef5e424ff
gurpreetkaur <gurpreetalliswell@gmail.com>
2023-04-21 15:07:11 +0930

    chances with win message

--- tictac.js
+++ tictac.js
@@ -10,9 +10,42 @@ for(var i=0 ; i<=9; i++){
 		if(clickedBox!=='' && clicks%2===0){
 		clicks= clicks+1;	
 		clickedBox.textContent = "X";
-			console.log(clicks)
+			if(box1.textContent ==="X" && box2.textContent==="X" && box3.textContent==="X"){
+				console.log(" X player Won!!!")
+			}else if(box4.textContent ==="X" && box5.textContent==="X" && box6.textContent==="X"){
+				console.log(" X player Won!!!")
+			}else if(box7.textContent ==="X" && box8.textContent==="X" && box9.textContent==="X"){
+				console.log(" X player Won!!!")
+			}else if(box1.textContent ==="X" && box4.textContent==="X" && box7.textContent==="X"){
+				console.log(" X player Won!!!")
+			}else if(box2.textContent ==="X" && box5.textContent==="X" && box8.textContent==="X"){
+				console.log(" X player Won!!!")
+			}else if(box3.textContent ==="X" && box6.textContent==="X" && box9.textContent==="X"){0.
+				console.log(" X player Won!!!")
+			}else if(box1.textContent ==="X" && box5.textContent==="X" && box9.textContent==="X"){
+				console.log(" X player Won!!!")
+			}else if(box3.textContent ==="X" && box5.textContent==="X" && box7.textContent==="X"){
+				console.log(" X player Won!!!")
+			}
 		}else{
 			clickedBox.textContent="O"
+			if(box1.textContent ==="O" && box2.textContent==="O" && box3.textContent==="O"){
+				console.log(" O player Won!!!")
+			}else if(box4.textContent ==="O" && box5.textContent==="O" && box6.textContent==="O"){
+				console.log(" O player Won!!!")
+			}else if(box7.textContent ==="O" && box8.textContent==="O" && box9.textContent==="O"){
+				console.log(" O player Won!!!")
+			}else if(box1.textContent ==="O" && box4.textContent==="O" && box7.textContent==="O"){
+				console.log(" O player Won!!!")
+			}else if(box2.textContent ==="O" && box5.textContent==="O" && box8.textContent==="O"){
+				console.log(" O player Won!!!")
+			}else if(box3.textContent ==="O" && box6.textContent==="O" && box9.textContent==="O"){
+				console.log(" O player Won!!!")
+			}else if(box1.textContent ==="O" && box5.textContent==="O" && box9.textContent==="O"){
+				console.log(" O player Won!!!")
+			}else if(box3.textContent ==="O" && box5.textContent==="O" && box7.textContent==="O"){
+				console.log(" O player Won!!!")
+			}
 			clicks= clicks+1;
 		}
 }
