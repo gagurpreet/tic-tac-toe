648e5eb50b9bb8112dba35f39e7153fc9106f9c9
gurpreetkaur <gurpreetalliswell@gmail.com>
2023-04-21 14:33:55 +0930

    adding for loop for 9 clicks to happen

--- tictac.js
+++ tictac.js
@@ -5,16 +5,22 @@ var clicks = 0
 board.addEventListener('click', function(event){
 	console.log(event.target)
 	var clickedBox= event.target
-	//for(var i =0 ; i<)
-	
-	if(clickedBox!==''){
-		clicks= clicks+1;
-		console.log(clicks)
-	}
-	clickedBox.textContent = "X";
-	if(clicks%=2){
-		clickedBox.textContent="O"
-	}
+for(var i=0 ; i<=9; i++){
+	clicks[i]
+		if(clickedBox!=='' && clicks%2===0){
+		clicks= clicks+1;	
+		clickedBox.textContent = "X";
+			console.log(clicks)
+		}else{
+			clickedBox.textContent="O"
+			clicks= clicks+1;
+		}
+}
+ //var vertical = document.querySelector('')
+
+ //console.log(vertical.id)
+
+
 })
 	
 		
