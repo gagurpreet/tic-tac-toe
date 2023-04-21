504d4984f29b328ba5c9ee58176dfaf7f353b461
gurpreetkaur <gurpreetalliswell@gmail.com>
2023-04-21 15:17:53 +0930

    chances with win message

--- tictac.js
+++ tictac.js
@@ -5,8 +5,7 @@ var clicks = 0
 board.addEventListener('click', function(event){
 	console.log(event.target)
 	var clickedBox= event.target
-for(var i=0 ; i<=9; i++){
-	clicks[i]
+
 		if(clickedBox!=='' && clicks%2===0){
 		clicks= clicks+1;	
 		clickedBox.textContent = "X";
@@ -48,12 +47,12 @@ for(var i=0 ; i<=9; i++){
 			}
 			clicks= clicks+1;
 		}
-}
+})
  //var vertical = document.querySelector('')
 
  //console.log(vertical.id)
 
 
-})
+
 	
 		
