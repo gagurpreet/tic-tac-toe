77843abbac5dc3e523edd7f9dea5cf6511ae2e71
gurpreetkaur <gurpreetalliswell@gmail.com>
2023-04-24 14:58:16 +0930

    Draw condition

--- tictac.js
+++ tictac.js
@@ -1,57 +1,63 @@
 	
 var board = document.querySelector('.board');
-var whoesTurn = 'X';
+var boxes = document.querySelector('#boxes')
 var winMsg = document.querySelector('#winMsg')
-var turn = 0;
+var turn= 0
+var whoesTurn = 'X'
+
 board.addEventListener('click', function(event){
 	var clickedBox= event.target
+
 	console.log( clickedBox)
-	if(clickedBox!=='' && whoesTurn ==='X' && turn%2===0){
+
+	if(clickedBox!=='' && turn%2===0 && whoesTurn==='X'){
 			
-			clickedBox.textContent = "X";
-			if(box1.textContent ==="X" && box2.textContent==="X" && box3.textContent==="X"){
-				console.log(" X player Won!!!")
-				winMsg.textContent= " X player Won!!!"
-			}else if(box4.textContent ==="X" && box5.textContent==="X" && box6.textContent==="X"){
-				winMsg.textContent= " X player Won!!!"
-			}else if(box7.textContent ==="X" && box8.textContent==="X" && box9.textContent==="X"){
-				winMsg.textContent= " X player Won!!!"
-			}else if(box1.textContent ==="X" && box4.textContent==="X" && box7.textContent==="X"){
-				winMsg.textContent= " X player Won!!!"
-			}else if(box2.textContent ==="X" && box5.textContent==="X" && box8.textContent==="X"){
-				winMsg.textContent= " X player Won!!!"
-			}else if(box3.textContent ==="X" && box6.textContent==="X" && box9.textContent==="X"){
-				winMsg.textContent= " X player Won!!!"
-			}else if(box1.textContent ==="X" && box5.textContent==="X" && box9.textContent==="X"){
-				winMsg.textContent= " X player Won!!!"
-			}else if(box3.textContent ==="X" && box5.textContent==="X" && box7.textContent==="X"){
-				winMsg.textContent= " X player Won!!!"
-			}
-			turn= turn+1;
-	}else{
-		if(clickedBox!=='' && !turn%2 === 0 )
-			clickedBox.textContent="O"
-				if(box1.textContent ==="O" && box2.textContent==="O" && box3.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}else if(box4.textContent ==="O" && box5.textContent==="O" && box6.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}else if(box7.textContent ==="O" && box8.textContent==="O" && box9.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}else if(box1.textContent ==="O" && box4.textContent==="O" && box7.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}else if(box2.textContent ==="O" && box5.textContent==="O" && box8.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}else if(box3.textContent ==="O" && box6.textContent==="O" && box9.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}else if(box1.textContent ==="O" && box5.textContent==="O" && box9.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}else if(box3.textContent ==="O" && box5.textContent==="O" && box7.textContent==="O"){
-					winMsg.textContent= " O player Won!!!"
-				}
-				turn = turn +1;
-	}
-	
-			
+		clickedBox.textContent = "X";
+		if(box1.textContent ==="X" && box2.textContent==="X" && box3.textContent==="X"){
+			console.log(" X player Won!!!")
+			winMsg.textContent= " X player Won!!!"
+		}else if(box4.textContent ==="X" && box5.textContent==="X" && box6.textContent==="X"){
+			winMsg.textContent= " X player Won!!!"
+		}else if(box7.textContent ==="X" && box8.textContent==="X" && box9.textContent==="X"){
+			winMsg.textContent= " X player Won!!!"
+		}else if(box1.textContent ==="X" && box4.textContent==="X" && box7.textContent==="X"){
+			winMsg.textContent= " X player Won!!!"
+		}else if(box2.textContent ==="X" && box5.textContent==="X" && box8.textContent==="X"){
+			winMsg.textContent= " X player Won!!!"
+		}else if(box3.textContent ==="X" && box6.textContent==="X" && box9.textContent==="X"){
+			winMsg.textContent= " X player Won!!!"
+		}else if(box1.textContent ==="X" && box5.textContent==="X" && box9.textContent==="X"){
+			winMsg.textContent= " X player Won!!!"
+		}else if(box3.textContent ==="X" && box5.textContent==="X" && box7.textContent==="X"){
+			winMsg.textContent= " X player Won!!!"
+		}
+		turn= turn+1;
+	}else if(clickedBox!=='' && !turn%2 === 0  && whoesTurn=== 'X'){
+		
+		clickedBox.textContent="O"
+		if(box1.textContent ==="O" && box2.textContent==="O" && box3.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}else if(box4.textContent ==="O" && box5.textContent==="O" && box6.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}else if(box7.textContent ==="O" && box8.textContent==="O" && box9.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}else if(box1.textContent ==="O" && box4.textContent==="O" && box7.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}else if(box2.textContent ==="O" && box5.textContent==="O" && box8.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}else if(box3.textContent ==="O" && box6.textContent==="O" && box9.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}else if(box1.textContent ==="O" && box5.textContent==="O" && box9.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}else if(box3.textContent ==="O" && box5.textContent==="O" && box7.textContent==="O"){
+			winMsg.textContent= " O player Won!!!"
+		}
+		turn = turn +1;
+	}else if(clickedBox!=='' && !turn%2 === 0  && whoesTurn==='X'||'O'){
+		if((box1.textContent ==="O" || box1.textContent ==="X") && (box2.textContent ==="O" || box2.textContent==="X" ) && (box3.textContent ==="O" || box3.textContent==="X") && (box4.textContent ==="O" || box4.textContent ==="X") && (box5.textContent ==="O" || box5.textContent ==="X") && (box6.textContent ==="O" || box6.textContent ==="X") && (box7.textContent ==="O" || box7.textContent ==="X") && (box8.textContent ==="O" || box8.textContent ==="X") && (box9.textContent ==="O" || box9.textContent ==="X")){
+			winMsg.textContent= "Match is draw!!"
+		}	
+	}		
 })
 
 	
