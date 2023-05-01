676e3b49f3742bfced2e5c1425118e31cd37ef48
gurpreetkaur <gurpreetalliswell@gmail.com>
2023-05-02 08:49:07 +0930

    Finally my luck is on my side :)

--- tictac.js
+++ tictac.js
@@ -3,17 +3,17 @@ var board = document.querySelector('.board');
 var boxes = document.querySelector('#boxes')
 var winMsg = document.querySelector('#winMsg')
 var turn= 0
-var whoesTurn = "X"
+var whoesTurn = 'X'
 var gameStatus = true
 
 board.addEventListener('click', function(event){
 	var clickedBox= event.target
 
 	console.log( clickedBox)
-
-	if(clickedBox !=='' && turn%2==0 && turn !== 9 && whoesTurn=== "X" && gameStatus !==false){
+	clickedBox.textContent = "X";
+	if(clickedBox !=='' && turn%2==0 && turn !== 8 && whoesTurn==='X' && gameStatus !==false){
 			
-		clickedBox.textContent = "X";
+		
 		if(box1.textContent ==="X" && box2.textContent==="X" && box3.textContent==="X"){
 			console.log(" X player Won!!!")
 			winMsg.textContent= " X player Won!!!"
@@ -43,7 +43,7 @@ board.addEventListener('click', function(event){
 		turn= turn+1;
 		return 
 
-	}else if(clickedBox!=='' && turn%2!== 0 && turn!== 9 && whoesTurn=== "X"  && gameStatus !==false){
+	}else if(clickedBox!=='' && turn%2!== 0 && turn!== 8 && whoesTurn=== 'X'  && gameStatus !==false){
 		
 		clickedBox.textContent="O"
 		if(box1.textContent ==="O" && box2.textContent==="O" && box3.textContent==="O"){
@@ -73,11 +73,10 @@ board.addEventListener('click', function(event){
 			gameStatus =false
 		}
 		turn = turn +1;
-		return;
-	}else if(gameStatus === true && turn === 9 ) {
-		if(
-			(box1.textContent ==="O" || box1.textContent ==="X") && (box2.textContent ==="O" || box2.textContent==="X" ) && (box3.textContent ==="O" || box3.textContent==="X") && (box4.textContent ==="O" || box4.textContent ==="X") && (box5.textContent ==="O" || box5.textContent ==="X") && (box6.textContent ==="O" || box6.textContent ==="X") && (box7.textContent ==="O" || box7.textContent ==="X") && (box8.textContent ==="O" || box8.textContent ==="X") && (box9.textContent ==="O" || box9.textContent ==="X")
-		){
+		
+	}else if(gameStatus ==true) {
+		((box1.textContent ==="O" || box1.textContent ==="X") && (box2.textContent ==="O" || box2.textContent==="X" ) && (box3.textContent ==="O" || box3.textContent==="X") && (box4.textContent ==="O" || box4.textContent ==="X") && (box5.textContent ==="O" || box5.textContent ==="X") && (box6.textContent ==="O" || box6.textContent ==="X") && (box7.textContent ==="O" || box7.textContent ==="X") && (box8.textContent ==="O" || box8.textContent ==="X") && (box9.textContent ==="O" || box9.textContent ==="X"))
+		{
 		winMsg.textContent= " Match is draw!! "
 		
 		}	
